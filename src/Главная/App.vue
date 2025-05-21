d4b1d3462af6e608fd7f4a146143a7febac6007c
bobiskj <borismachkalan@gmail.com>
2025-05-21 13:12:20 +0300

    Initial commit: Vue.js project

---
+++ src/Главная/App.vue
@@ -0,0 +1,109 @@
+<template>
+  <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+    <div class="container-fluid row">
+      <div class="col col-1"></div>
+      <div class="col col-10">
+        <h5 class="white-heading">Hauthink</h5>
+      </div>
+    </div>
+  </nav>
+
+  <div class="bg-image-container d-flex justify-content-center align-items-center">
+<div class="d-flex flex-column align-items-center col-8">
+      <h6 style="
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 20px;
+text-align: center;
+letter-spacing: 2px;
+text-transform: uppercase;
+color: #C0C0C0;
+      ">PHOTOGRAPHER & FILMMAKER</h6>
+        <h1 style="
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 400;
+font-size: 54px;
+line-height: 59px;
+text-align: center;
+color: #FFFFFF;
+        ">Aperture Studios</h1>
+        <h5 style="
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 20px;
+text-align: center;
+color: #C0C0C0;
+        " >
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Suspendisse varius enim in eros elementum tristique. 
+Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. 
+Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
+          
+        </h5>
+
+    </div>
+    
+    </div>
+
+
+<div style="background-color: black;" class="d-flex flex-column align-items-center">
+  <h2 style="
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 400;
+font-size: 40px;
+line-height: 42px;
+text-align: center;
+color: #FFFFFF;
+  ">What we do.</h2>
+<h4 style="
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 400;
+font-size: 23px;
+line-height: 30px;
+text-align: center;
+color: #C0C0C0;
+">The areas that we're specialized in.</h4>
+    <div class="d-flex justify-content-center col-10 ">
+      <img src="@/assets/1.png" style="width: 50%; height: 50%;">
+      <img src="@/assets/2.png" style="width: 50%; height: 50%;">
+    </div>
+    
+    <div class="d-flex justify-content-center col-10">
+      <img src="@/assets/3.png" style="width: 50%; height: 50%;">
+      <img src="@/assets/4.png" style="width: 50%; height: 50%;">
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+/*div {
+  border: 1px solid rgb(255, 255, 255) !important;
+  padding: 4px;
+  margin: 2px;
+}*/
+.white-heading {
+  font-weight: bold;
+  color: white;
+  margin: 0.5em 0;
+}
+
+.bg-image-container {
+  width: 100%;
+  height: 100vh; 
+  background-image: url('@/assets/gggg.png');
+  background-size: cover;
+}
+</style>
